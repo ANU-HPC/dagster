8d5eea939a0cd7cdd0bf9deed7e59be6c59068a0
Mark <markburgess1989@gmail.com>
2022-06-10 12:10:22 +1000

    add unit test

--- dagster/tests/minimal/cleanup.sh
+++ dagster/tests/minimal/cleanup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "CLEANING UP"
 for d in ./*/ ;
-  do (echo "$d" && cd "$d" && rm -f *.sols && rm -f *.true_sols);
+  do (echo "$d" && cd "$d" && rm -f *.sols && rm -f *.true_sols && rm -f *.check);
 done
